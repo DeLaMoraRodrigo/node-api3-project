76b965990dec149daa9e8f2ebe82c85fbcf65688
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-05-07 14:35:04 -0700

    Made port dynamic with process.env.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors")
 
 const server = express();
 
+const port = process.env.PORT || 8000;
+
 server.use(express.json());
 server.use(cors());
 
@@ -27,8 +29,8 @@ function logger(req, res, next) {
 
 server.use(logger)
 
-server.listen(8000, () => {
-  console.log("Api is running")
+server.listen(port, () => {
+  console.log(`Api is running on ${port}`)
 })
 
 module.exports = server;
